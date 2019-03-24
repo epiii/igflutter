d387eac28aee937bb6d2ca9f9eaf5395bdb5fb7c
elfri.app <gue.elfrianto@gmail.com>
2019-03-25 02:03:26 +0700

    initiate proj

---
+++ lib/__main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new Text("ashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaapppp boss.....", textDirection:TextDirection.ltr)
+  );
+}
