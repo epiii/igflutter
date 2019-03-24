9f5137648e2c58c14838758fb4834069ba84b79e
elfri.app <gue.elfrianto@gmail.com>
2019-03-25 04:45:11 +0700

    add "top bar"

--- lib/__main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main(){
-  runApp(
-    new Text("ashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaappppashiaapppp boss.....", textDirection:TextDirection.ltr)
-  );
-}
